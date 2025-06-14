5a8c0aad19d5fbf532941d66acd3d06cebd4e798
AkSpy24 <nargisofficial32@gmail.com>
2025-06-14 22:19:44 +0600

    user registration endpoint has been created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from users.serializers import CustomUserRegisterSerializer
+
+
+class CustomUserRegisterView(generics.CreateAPIView):
+    model = get_user_model()
+    serializer_class = CustomUserRegisterSerializer
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        return Response(status=status.HTTP_201_CREATED)
